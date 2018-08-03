357c2666aa238a1bf113dde46276be3c86867666
Simon Gomizelj <simon@vodik.xyz>
2018-08-03 18:13:44 -0400

    Prepare v0.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='asyncitertools',
-    version='0.0.2',
+    version='0.0.3',
     author='Simon Gomizelj',
     author_email='simon@vodik.xyz',
     maintainer='Simon Gomizelj',
